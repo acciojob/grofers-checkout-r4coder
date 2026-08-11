bfb1753fef477427bf26b947597d39764087222d
r4coder <ritheeshk2003@gmail.com>
2026-08-11 09:44:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,30 @@
 const getSumBtn = document.createElement("button");
+
 getSumBtn.append("Get Total Price");
+
 document.body.appendChild(getSumBtn);
 
+
 const getSum = () => {
-//Add your code here
-  
+
+    const prices = document.querySelectorAll(".price");
+
+    let sum = 0;
+
+    for (let i = 0; i < prices.length; i++) {
+        sum += Number(prices[i].innerText);
+    }
+
+    const tr = document.createElement("tr");
+
+    const td = document.createElement("td");
+
+    td.innerText = sum;
+
+    tr.appendChild(td);
+
+    document.querySelector("table").appendChild(tr);
 };
 
-getSumBtn.addEventListener("click", getSum);
 
+getSumBtn.addEventListener("click", getSum);
